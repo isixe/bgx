f3bde3984c25586ebc6dd4e6e8a0a7c44ac2fa4c
Shrimp Task Manager <shrimp@task-manager.local>
2026-05-13 23:06:03 +0800

    ✨ feat(models): Add ModelScope fallback URLs for model downloads.H

--- src/config/models.ts
+++ src/config/models.ts
@@ -1,15 +1,19 @@
 import type { ModelConfig } from '../types/app';
 
 const HF_HOST = 'https://huggingface.co';
-const HF_MIRROR_HOST = '';
+const ML_HOST = 'https://www.modelscope.cn/models';
 
-const hfUrl = (repo: string, path: string) => ({
+const loadUrl = (repo: string, path: string) => ({
   downloadUrl: `${HF_HOST}/${repo}/resolve/main/${path}?download=true`,
-  feedbackUrl: ``
+  feedbackUrl: `${ML_HOST}/${repo}/resolve/master/onnx/${path}`
 });
 
-const REMBG_REPO = 'tomjackson2023/rembg';
-const BRIA_RMBG_REPO = 'briaai/RMBG-1.4';
+const HF_REMBG = 'tomjackson2023/rembg';
+const HF_BRIA_RMBG_1_4 = 'briaai/RMBG-1.4';
+
+const ML_SHIERTIER_REMBG = "shiertier/rembg"
+const ML_AI_MODEL_SCOPE_1_4 = "AI-ModelScope/RMBG-1.4"
+const ML_AI_MODEL_SCOPE_2_0 = "AI-ModelScope/RMBG-2.0"
 
 export const MODELS: ModelConfig[] = [
   {
@@ -19,7 +23,7 @@ export const MODELS: ModelConfig[] = [
     size: '~4MB',
     filename: 'u2netp.onnx',
     descKey: 'model.u2netp.desc',
-    ...hfUrl(REMBG_REPO, 'u2netp.onnx'),
+    downloadUrl: `${HF_HOST}/${HF_REMBG}/resolve/main/u2netp.onnx?download=true`,
     feedbackUrl: "https://unpkg.com/@rmbg/model-u2netp@0.0.1/u2netp.onnx",
   },
   {
@@ -29,7 +33,8 @@ export const MODELS: ModelConfig[] = [
     size: '~168MB',
     filename: 'u2net.onnx',
     descKey: 'model.u2net.desc',
-    ...hfUrl(REMBG_REPO, 'u2net.onnx')
+    downloadUrl: `${HF_HOST}/${HF_REMBG}/resolve/main/u2net.onnx?download=true`,
+    feedbackUrl: `${ML_HOST}/${ML_SHIERTIER_REMBG}/resolve/master/u2net.onnx`,
   },
   {
     id: 'u2net_human_seg',
@@ -38,7 +43,8 @@ export const MODELS: ModelConfig[] = [
     size: '~168MB',
     filename: 'u2net_human_seg.onnx',
     descKey: 'model.u2net_human_seg.desc',
-    ...hfUrl(REMBG_REPO, 'u2net_human_seg.onnx')
+    downloadUrl: `${HF_HOST}/${HF_REMBG}/resolve/main/u2net_human_seg.onnx?download=true`,
+    feedbackUrl: `${ML_HOST}/${ML_SHIERTIER_REMBG}/resolve/master/u2net_human_seg.onnx`,
   },
   {
     id: 'silueta',
@@ -47,7 +53,8 @@ export const MODELS: ModelConfig[] = [
     size: '~42MB',
     filename: 'silueta.onnx',
     descKey: 'model.silueta.desc',
-    ...hfUrl(REMBG_REPO, 'silueta.onnx')
+    downloadUrl: `${HF_HOST}/${HF_REMBG}/resolve/main/silueta.onnx?download=true`,
+    feedbackUrl: `${ML_HOST}/${ML_SHIERTIER_REMBG}/resolve/master/silueta.onnx`,
   },
   {
     id: 'isnet-general-use',
@@ -56,7 +63,8 @@ export const MODELS: ModelConfig[] = [
     size: '~170MB',
     filename: 'isnet-general-use.onnx',
     descKey: 'model.isnet-general-use.desc',
-    ...hfUrl(REMBG_REPO, 'isnet-general-use.onnx')
+    downloadUrl: `${HF_HOST}/${HF_REMBG}/resolve/main/isnet-general-use.onnx?download=true`,
+    feedbackUrl: `${ML_HOST}/${ML_SHIERTIER_REMBG}/resolve/master/isnet-general-use.onnx`,
   },
   {
     id: 'isnet-anime',
@@ -65,7 +73,8 @@ export const MODELS: ModelConfig[] = [
     size: '~168MB',
     filename: 'isnet-anime.onnx',
     descKey: 'model.isnet-anime.desc',
-    ...hfUrl(REMBG_REPO, 'isnet-anime.onnx')
+    downloadUrl: `${HF_HOST}/${HF_REMBG}/resolve/main/isnet-anime.onnx?download=true`,
+    feedbackUrl: `${ML_HOST}/${ML_SHIERTIER_REMBG}/resolve/master/isnet-anime.onnx`,
   },
   {
     id: 'bria-rmbg-quantized',
@@ -74,7 +83,8 @@ export const MODELS: ModelConfig[] = [
     size: '~44MB',
     filename: 'model_quantized.onnx',
     descKey: 'model.bria-rmbg-quantized.desc',
-    ...hfUrl(BRIA_RMBG_REPO, 'onnx/model_quantized.onnx')
+    downloadUrl: `${HF_HOST}/${HF_BRIA_RMBG_1_4}/resolve/main/onnx/model_quantized.onnx?download=true`,
+    feedbackUrl: `${ML_HOST}/${ML_AI_MODEL_SCOPE_1_4}/resolve/master/onnx/model_quantized.onnx`,
   },
   {
     id: 'bria-rmbg-fp16',
@@ -83,7 +93,8 @@ export const MODELS: ModelConfig[] = [
     size: '~88MB',
     filename: 'model_fp16.onnx',
     descKey: 'model.bria-rmbg-fp16.desc',
-    ...hfUrl(BRIA_RMBG_REPO, 'onnx/model_fp16.onnx')
+    downloadUrl: `${HF_HOST}/${HF_BRIA_RMBG_1_4}/resolve/main/onnx/model_fp16.onnx?download=true`,
+    feedbackUrl: `${ML_HOST}/${ML_AI_MODEL_SCOPE_1_4}/resolve/master/onnx/model_fp16.onnx`,
   },
   {
     id: 'bria-rmbg',
@@ -92,6 +103,7 @@ export const MODELS: ModelConfig[] = [
     size: '~176MB',
     filename: 'model.onnx',
     descKey: 'model.bria-rmbg.desc',
-    ...hfUrl(BRIA_RMBG_REPO, 'onnx/model.onnx')
-  },
+    downloadUrl: `${HF_HOST}/${HF_BRIA_RMBG_1_4}/resolve/main/onnx/model.onnx?download=true`,
+    feedbackUrl: `${ML_HOST}/${ML_AI_MODEL_SCOPE_1_4}/resolve/master/onnx/model.onnx`,
+  }
 ];
